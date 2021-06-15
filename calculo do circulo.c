055613947ef7992c44fd253b3f9c16414730ef0e
Victor Hugo Constantino Lozano <lozano_victor_@hotmail.com>
2021-06-15 19:39:40 -0300

    Update calculo do circulo.c

--- calculo do circulo.c
+++ calculo do circulo.c
@@ -3,15 +3,17 @@
 #include <math.h>
 
 double main() {
-    double raio=0;
-    double area=0;
-    double pi=3.14;
+    float raio=0;
+    float area=0;
+    float pi=3.14;
     printf("Calculo da area de um circulo\n");
     printf("Entre o valor do raio:\n");
-    while (scanf("%f", &raio)<=0){
+    scanf("%f", &raio);
+    while (raio<=0){
         printf("Valor invalido, por favor insira um valor valido:\n");
+        scanf("%f", &raio);
     }
     area=(raio*raio)*pi;
-    printf("Area do circulo: %f", &area);
+    printf("Area do circulo: %f", area);
     return 0;
 }
